7c256a25df0129ab1671b5faaecc3704b974e5e6
Iuliia Chipsanova <iuliia.chipsanova@gritlab.ax>
2023-09-20 11:43:38 +0300

    configuration for https

---
+++ src/main/java/gritlab/products/config/WebConfig.java
@@ -0,0 +1,18 @@
+package gritlab.products.config;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+        return container -> {
+            container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
+        };
+    }
+}
